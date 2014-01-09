36a3ad4f8d0dfaba179930f035ea99cc57b33af0
rusakovprz <rusakovprz@rambler.ru>
2014-01-09 17:32:18 +0400

    ref #14

--- uart.c
+++ uart.c
@@ -6,6 +6,7 @@
   
 */
 
+#include <avr/interrupt.h>
 #include <avr/io.h>
 #include "uart.h"
 
@@ -19,19 +20,31 @@ void init_uart()
   UBRR0H = 129 >> 8;
   
   // 8 бит данных.
-  UCSR0C = ( 1 << UMSEL01 ) | ( 0 << UCSZ02 ) | ( 1 << UCSZ01 ) | ( 1 << UCSZ00 );
+  UCSR0C |= ( 1 << UMSEL01 ) | ( 0 << UCSZ02 ) | ( 1 << UCSZ01 ) | ( 1 << UCSZ00 );
 
   // Режим с отключенным контролем четности.
-  UCSR0C = ( 1 << UMSEL01 ) | ( 0 << UPM01 ) | ( 0 << UPM00 ) ;
+  UCSR0C |= ( 1 << UMSEL01 ) | ( 0 << UPM01 ) | ( 0 << UPM00 ) ;
   
   // Режим с одним стоп-битом.
-  UCSR0C = ( 1 << UMSEL01 ) | ( 0 << USBS0 );
+  UCSR0C |= ( 1 << UMSEL01 ) | ( 0 << USBS0 );
 
   // Разрешение приема и передачи данных по UART.
-  UCSR0B = ( 1 << TXEN0 ) | ( 1 << RXEN0 );
+  UCSR0B |= ( 1 << TXEN0 ) | ( 1 << RXEN0 );
 
   // Разрешение прерывания по завершению приема, по освобождению регистра данных передтчика.
-  UCSR0B = ( 1 << RXCIE0 ) | ( 1 << UDRIE0 );
+  UCSR0B |= ( 1 << RXCIE0 ) | ( 1 << UDRIE0 );
+
+}
+
+
+ISR( USART_RX_vect )
+{
+
+}
+
+
+ISR( USART_UDRE_vect )
+{
 
 }
 
